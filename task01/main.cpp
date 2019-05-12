c16f84165255d64183562a2597b210d1d1cbd7b9
Nastya <48887225+tropich@users.noreply.github.com>
2019-05-12 21:26:43 +0300

    Update main.cpp

--- task01/main.cpp
+++ task01/main.cpp
@@ -4,19 +4,12 @@
 
 int main()
 {
-    Complex a, b(2.0), c(1.0, 1.0);
-
-    std::cin >> a;
-    std::cout << a << ", " << b << ", abs: " << b.abs() << std::endl;
-
-    a = b + c.conj();
-    a = a * Complex(2.0);
+    C a, b(2.0), c(1.0, 1.0);
+    a = a * C(2.0);
     a += c;
     c = a + 2 * b;
 
     std::cout << a << ", " << c << std::endl;
-
-    // Add your usage examples
 
     return 0;
 }
